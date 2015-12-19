02755cdcbd9741c3a220417f3e0cd054eaed8755
yutaka <shigure@refy.net>
2015-12-19 14:11:55 +0900

    listen quickly

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 var fs = require('fs')
   , path = require('path');
 
-var express = require('express');
-var app = express();
-
-var files = fs.readdirSync(path.resolve('api'));
-var methods = ['get', 'post', 'put', 'delete'];
-
-for(var i = 0; i < files.length; ++i){
-  var api = require(path.resolve(path.join('api', files[i])));
-  var m = files[i].match(/(.*?)\.js/);
-  for(var j = 0; j < methods.length; ++j){
-    if(api[methods[i]]) app[methods[i]]('/api/' + m[1], api[methods[i]]);
+function run(dir, port){
+  if(port == undefined){
+    port = dir;
+    dir = 'api';
   }
+
+  var express = require('express');
+  var app = express();
+  
+  var files = fs.readdirSync(path.resolve(dir));
+  var methods = ['get', 'post', 'put', 'delete'];
+  
+  for(var i = 0; i < files.length; ++i){
+    var api = require(path.resolve(path.join(dir, files[i])));
+    var m = files[i].match(/(.*?)\.js/);
+    for(var j = 0; j < methods.length; ++j){
+      if(api[methods[i]]) app[methods[i]]('/api/' + m[1], api[methods[i]]);
+    }
+  }
+
+  app.listen(port);
+  return app;
 }
 
-module.exports = app;
+module.exports = run;
